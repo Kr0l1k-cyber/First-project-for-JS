21025e52666f9b89316671e3cf446806b6c6cf23
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-03-26 20:26:37 +0300

    Добавил переменные

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log ('Первое сообщение в консоли');
 alert ('Heelo my first script');
+const title = 1;
+let screens = 4;
+const screenPrice = 2;
+let rollback = 5;
+const fullPrice = 3;
+let adaptive = 6;
