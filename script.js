75728e729e51c0248807d6e5bc056bb96860ee6e
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-03-27 22:19:49 +0300

    Работа сконсолью

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 const title = 'Мой первый проэкт';
+console.log(typeof title);
 let screens = 'Простые, Сложные, Интерактивные';
+console.log(screens. toLowerCase(),screens. split(','));
+console.log(screens. split(','));
+console.log(screens. length);
 const screenPrice = 1000;
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 let rollback = 71;
 const fullPrice = 10000;
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(typeof fullPrice);
 let adaptive = true;
+console.log(typeof adaptive);
