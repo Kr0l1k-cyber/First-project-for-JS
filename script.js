5925e92f41e8b77d6a440135d179e9a8def91e6a
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-05 21:36:09 +0300

    отремонтировал перебор

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // const title = prompt('Как называется Ваш проект?')
 
 // const screens = prompt('Какие типы экранов нужно разработать?')
@@ -9,12 +9,27 @@ const scrrenPrice = +prompt('Сколько будет стоить данная
 // console.log(title, screens, scrrePrice, adaptiv);
 // //расчте стоимости
 
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
+// const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
+// const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
 
 const fullPrice = Number(scrrenPrice + servicePrice1 + servicePrice2);
 
-const servicePercentPrice =  (Math.ceil(parseFloat(fullPrice * (100 - 11))) / 100);
+const servicePercentPrice = Math.ceil(parseFloat(fullPrice * (100 - 11))) / 100;
 console.log(servicePercentPrice);
+
+switch (true) {
+    case servicePercentPrice > 30000:
+        console.log('Даем скидку в 10%');
+        break
+    case 15000 < servicePercentPrice && servicePercentPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break
+    case 0 < servicePercentPrice && servicePercentPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break
+    default:
+        console.log('Что то пошло не так');
+}
+
