79bcd8204052bdf98c326da332a63f0ade25092a
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-05 20:31:03 +0300

    добавил вопросы к пользавотелю цена

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-const title = prompt('Как называется Ваш проект?')
+// const title = prompt('Как называется Ваш проект?')
 
-const screens = prompt('Какие типы экранов нужно разработать?')
+// const screens = prompt('Какие типы экранов нужно разработать?')
 
-const scrrePrice = +prompt('Сколько будет стоить данная работа?')
+// const scrrePrice = +prompt('Сколько будет стоить данная работа?')
 
-const adaptiv = confirm('Нужен ли адаптив на сайте?')
-console.log(title, screens, scrrePrice, adaptiv);
+// const adaptiv = confirm('Нужен ли адаптив на сайте?')
+// console.log(title, screens, scrrePrice, adaptiv);
+// //расчте стоимости
+
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
