e96a285043e8cd19f2ac268e98a0a4681dc5a046
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-05 20:48:38 +0300

    добавил переменную сложения всех сумм

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // const screens = prompt('Какие типы экранов нужно разработать?')
 
-// const scrrePrice = +prompt('Сколько будет стоить данная работа?')
+const scrrenPrice = +prompt('Сколько будет стоить данная работа?')
 
 // const adaptiv = confirm('Нужен ли адаптив на сайте?')
 // console.log(title, screens, scrrePrice, adaptiv);
@@ -13,3 +13,7 @@ const service1 = prompt('Какой дополнительный тип услу
 const servicePrice1 = +prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
+
+const fullPrice = Number(scrrenPrice + servicePrice1 + servicePrice2);
+
+console.log(fullPrice);
