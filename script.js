5caa49605ea4c2a6fbb2a2d4db965a7089eb208d
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-05 20:26:10 +0300

    старт 3его урока

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 
-const title = 'Мой первый проэкт';
-console.log(typeof title);
-let screens = 'Простые, Сложные, Интерактивные';
-console.log(screens. toLowerCase(),screens. split(','));
-console.log(screens. length);
-const screenPrice = 1000;
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-let rollback = 71;
-const fullPrice = 10000;
-console.log(fullPrice * (rollback/100));
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-console.log(typeof fullPrice);
-let adaptive = true;
-console.log(typeof adaptive);
+const title = prompt('Как называется Ваш проект?')
+
+const screens = prompt('Какие типы экранов нужно разработать?')
+
+const scrrePrice = +prompt('Сколько будет стоить данная работа?')
+
+const adaptiv = confirm('Нужен ли адаптив на сайте?')
+console.log(title, screens, scrrePrice, adaptiv);
