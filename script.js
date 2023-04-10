3395326f02d1272f3956b66e9d731840c57f804e
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-10 21:38:16 +0300

    add function getServicePercentPrices

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 let roolback =10;
-// let fullPrice = Number(scrrenPrice + servicePrice1 + servicePrice2);
-// let servicePercentPrice = fullPrice - (fullPrice * (roolback / 100));
+let fullPrice = Number(scrrenPrice + servicePrice1 + servicePrice2);
+let servicePercentPrice = fullPrice - (fullPrice * (roolback / 100));
 
 const showTypeof = function(variable) {
     console.log(variable, typeof variable);
@@ -26,15 +26,39 @@ const getRoolBackMessage = function(price) {
     }
 }
 // Решаем задачки
+// возвращает сумму всех доп услуг
 const getAllServicePrices = function(dopPrice1, dopPrice2) {
     return dopPrice1 + dopPrice2
-}
+};
+// меняет фулпрайс
 function getFullPrice(a, b) {
     return a + b
-}
+};
+// меняет титл
+function getTitle(title) {
+    if (typeof title !== 'string') {
+        return title;
+        }
+    
+        title = title.trim();
+        return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+    };
+
+const getServicePercentPrices = function(price, discountPercentage) {
+    if (discountPercentage < 0 || discountPercentage > 100) {
+        throw new Error('Неверное значение');
+      }
+      
+      const discountAmount = price * (discountPercentage / 100);
+      const discountedPrice = price - discountAmount;
+      
+      return discountedPrice;
+};
 
 let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-let fullPrice = getFullPrice(scrrenPrice, allServicePrices)
+fullPrice = getFullPrice(scrrenPrice, allServicePrices);
+title = getTitle(title);
+servicePercentPrice = getServicePercentPrices(fullPrice, roolback)
 
 showTypeof(title);
 showTypeof(scrrenPrice);
@@ -43,6 +67,7 @@ showTypeof(adaptiv);
 console.log(allServicePrices);
 console.log(getRoolBackMessage(fullPrice));
 console.log(fullPrice);
+console.log(servicePercentPrice);
 console.log(typeof title);
 console.log(typeof scrrenPrice);
 console.log(typeof adaptiv);
